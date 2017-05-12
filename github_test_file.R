3113ae7ae5b923099b23ba3dc14d56275af0ad49
AjayOza <ajay.oza@hse.ie>
2017-05-12 13:18:41 +0100

    Test changes

--- github_test_file.R
+++ github_test_file.R
@@ -1,4 +1,4 @@
-eye_colour <- c("blue", "brown", "brown", "blue")
+eye_colour <- as.factor(c("blue", "brown", "brown", "blue")) #I am a pedant
 height <- c(1.9, 1.8, 1.9, 1.7)
 df1 <- data.frame(eye_colour, height)
 t.test(data= df1, height ~ eye_colour)
